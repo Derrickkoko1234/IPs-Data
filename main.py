3f41cef8ceee2a8b7864a0f032e297472885a345
Derrick Koko <kokoderrick3@gmail.com>
2022-05-04 19:24:27 +0100

    Update

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ print('Generating IPs...')
 list_of_ips = []
 for i in range(0, 1):
     first = str(1)
-    for j in range(0, 5):
+    for j in range(0, 6):
         second = str(j)
         print('Done')
-        for k in range(0, 255):
+        for k in range(0, 256):
             third = str(k)
-            for l in range(0, 255):
+            for l in range(0, 256):
                 forth = str(l)
                 ip = first + '.' + second + '.' + third + '.' + forth
                 file = open('ips.txt', 'a')
